df6e6fb460e05b8da6f09930d1cfa9efccade49c
aswanthmc <mcaswanth5@gmail.com>
2024-12-18 17:35:48 +0530

    add pool

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const pool =require('./db')
 
 var registerRouter = require('./routes/register');
 var usersRouter = require('./routes/users');
