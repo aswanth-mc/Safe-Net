57814333a75ec52733954396c8b362688d2af71d
aswanthmc <mcaswanth5@gmail.com>
2024-12-19 10:22:03 +0530

    adding gitignore

--- routes/register.js
+++ routes/register.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 const pool =require("../db");
 
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('register', { title: 'Express' });
+  res.render('register');
 });
+
 
 module.exports = router;
