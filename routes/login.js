8680b3967dccc2f7d9c225bede341982b0ce2e38
aswanthmc <mcaswanth5@gmail.com>
2024-12-22 20:11:37 +0530

    trying to connect home page

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,34 @@
 var express = require('express');
 var router = express.Router();
-const pool =require("../db");
+const pool = require('../db');
 
-
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
+// Render login page
+router.get('/', function (req, res, next) {
   res.render('login');
 });
 
+// Handle login
+router.post('/', async (req, res) => {
+  try {
+    const { email, pass } = req.body;
+    
+
+    const query = 'SELECT * FROM autho WHERE mail = $1 AND password = $2';
+    const values = [email, pass];
+
+    const result = await pool.query(query, values);
+
+    if (result.rows.length > 0) {
+      console.log("Login successful!");
+      res.redirect("/home"); 
+    } else {
+      console.log("Invalid credentials.");
+      res.status(401).send("Invalid username or password. Please try again.");
+    }
+  } catch (error) {
+    console.error("Error during login:", error.message);
+    res.status(500).send("Internal server error");
+  }
+});
 
 module.exports = router;
