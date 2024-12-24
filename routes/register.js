39e5c1c4c052ebe4871d096a907140e9f124d15a
aswanthmc <mcaswanth5@gmail.com>
2024-12-24 12:45:48 +0530

    adding password hashing

--- routes/register.js
+++ routes/register.js
@@ -1,3 +1,6 @@
+const bcrypt =require ('bcryptjs')
+
+
 var express = require('express');
 var router = express.Router();
 const pool =require("../db");
@@ -5,9 +8,35 @@ const pool =require("../db");
 
 
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('register');
+});
+
+router.post('/',async(req,res)=>{
+  const { name,mail,no,designation,dep,eid,dis,oadd,pass,cpass }=req.body;
+
+  if (pass !=cpass){
+    return res.status(400).send('password do not match');
+
+  }
+  const Password = bcrypt.hashSync(pass,10);
+  console.log('hased password',Password);
+  
+
+  console.log({
+    name,
+    mail,
+    no,
+    designation,
+    dep,
+    eid,
+    dis,
+    oadd,
+    Password
+  });
+
 });
 
 
