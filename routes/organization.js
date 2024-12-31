bd237ae004fd3966221669aa342837520e3ddd94
aswanthmc <mcaswanth5@gmail.com>
2024-12-31 20:03:49 +0530

    adding category status date into the table

--- routes/organization.js
+++ routes/organization.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var pool =require("../db")
 
 // Render home page
-router.get('/', function a (req, res) {
-  res.render("organization");
-
+router.get('/', async function a (req, res) {
   try {
-    const query ='select name,email,phone,location from rescue_club'
+    const query = `SELECT name, location, phone, email FROM rescue_club`;
+    const { rows } = await pool.query(query);
+
+    res.render('organization', { data: rows });
   } catch (error) {
-    
+    console.error('Error fetching sorted data:', error.message);
+    res.status(500).send('An error occurred while fetching data.');
   }
 });
-
 module.exports = router;
