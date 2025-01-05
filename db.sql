4149f1cfe044210c337394ba8810572fc9836052
aswanth-mc <mcaswanth5@gmail.com>
2025-01-05 09:24:44 +0530

    change emp_id as primary key

--- db.sql
+++ db.sql
@@ -1,14 +1,14 @@
 create table authority(
-     id SERIAL PRIMARY KEY,
+     employee_id VARCHAR PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     phone_number VARCHAR(15) NOT NULL,
     designation VARCHAR(100),
     department VARCHAR(100),
-    employee_id VARCHAR(50),
     district VARCHAR(100),
     office_address TEXT,
     id_proof TEXT,
     photo TEXT,
-    password_hash TEXT NOT NULL
+    password_hash TEXT NOT NULL,
+      verified BOOLEAN DEFAULT NULL
 );
