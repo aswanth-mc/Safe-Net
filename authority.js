9e54455ae703d96c91fa9c5795ed69e54c75bd63
aswanthmc <mcaswanth5@gmail.com>
2025-01-09 21:12:25 +0530

    arrange

--- authority.js
+++ authority.js
@@ -1,9 +1,9 @@
 const express = require ('express');
 const path = require ('path');
 
+var loginRouter =require('./routes/authority/login');
 var registerRouter = require('./routes/authority/register');
 var homeRouter = require('./routes/authority/home');
-var loginRouter =require('./routes/authority/login')
 var volunteerRouter =require('./routes/authority/volunteer');
 var call_vehicleRouter =require('./routes/authority/call_vehicle');
 var call_volunteerRouter =require('./routes/authority/call_volunteer');
@@ -16,15 +16,16 @@ var shelterRouter =require('./routes/authority/shelter');
 const authority=express();
 
 authority.use(express.static(path.join(__dirname, 'public')));
+authority.set('views', path.join(__dirname, 'views'));
+authority.set('view engine', 'hbs');
 authority.use(express.json());
 authority.use(express.urlencoded({ extended: false }));
 
-authority.set('views', path.join(__dirname, 'views'));
-authority.set('view engine', 'hbs');
+
 
 authority.use('/register', registerRouter);
 authority.use('/home', homeRouter);
-authority.use('/authority-login',loginRouter);
+authority.use('/login',loginRouter);
 authority.use('/volunteer',volunteerRouter);
 authority.use('/shelter',shelterRouter);
 authority.use('/call_vehicle',call_vehicleRouter);
