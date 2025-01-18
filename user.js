b965047add2cd1a70a2178516589f5ca471adb2f
aswanthmc <mcaswanth5@gmail.com>
2025-01-18 21:44:52 +0530

    initilize login router

--- user.js
+++ user.js
@@ -3,6 +3,7 @@ const path = require ('path');
 
 
 var registerRouter =require('./routes/user/register')
+var loginRouter = require('./routes/user/login')
 var organizationRouter =require('./routes/user/organization')
 var vehicleRouter =require('./routes/user/vehicle')
 
@@ -16,6 +17,7 @@ user.use(express.json());
 user.use(express.urlencoded({ extended: false }));
 
 user.use('/register',registerRouter);
+user.use('/login',loginRouter)
 user.use('/organization',organizationRouter);
 user.use('/vehicle',vehicleRouter);
 
